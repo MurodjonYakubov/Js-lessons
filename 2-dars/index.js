bb1dfab71dfb3f4cb533dd759bc2b9fae533b048
MurodjonYakubov <yakubovmurodjon85@gmail.com>
2025-04-07 21:23:26 +0500

    Mening JS-darslarim!

---
+++ 2-dars/index.js
@@ -0,0 +1,8 @@
+//                                  2 - darsda o'tganlarimiz
+
+//  Dars | Type Conversions | Operators | Comparison | Logical Operators 
+//  Type convirsions
+//  Operators +, -, *, /, %, (), =, ++, --, **
+//  Comparision ==, ===, <, >, <=, >=
+//  Logical Operations ||, &&, !, ??
+//  Difference between ?? and ||
