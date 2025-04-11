64ed77bf79cec8d2ecbe01fc03fc8010f4caec1e
MurodjonYakubov <yakubovmurodjon85@gmail.com>
2025-04-11 14:52:23 +0500

    My JS - lessons and homeworks!

--- 2-dars/index.js
+++
@@ -1,8 +0,0 @@
-//                                  2 - darsda o'tganlarimiz
-
-//  Dars | Type Conversions | Operators | Comparison | Logical Operators 
-//  Type convirsions
-//  Operators +, -, *, /, %, (), =, ++, --, **
-//  Comparision ==, ===, <, >, <=, >=
-//  Logical Operations ||, &&, !, ??
-//  Difference between ?? and ||
